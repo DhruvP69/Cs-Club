e62d9a7edf1f119e69b80fa36de9f977586b7ab3
DhruvP69 <138643120+DhruvP69@users.noreply.github.com>
2023-10-13 20:43:58 -0400

    Add files via upload
    The start

---
+++ CC1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class CC1{
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        String word;
+        double s;
+        double L;
+        word=input.nextLine();
+        System.out.println(word);
+        
+
+    }
+}
