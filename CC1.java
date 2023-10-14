0daab574c1274659330a26a2fc5ece80be0bc91b
DhruvP69 <138643120+DhruvP69@users.noreply.github.com>
2023-10-13 22:46:07 -0400

    Update CC1.java
    Final

--- CC1.java
+++ CC1.java
@@ -3,10 +3,28 @@ public class CC1{
     public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
         String word;
-        double s;
+        double S;
         double L;
+        int words=0;
+        int sentances=0;
+        int index;
         word=input.nextLine();
-        System.out.println(word);
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) == ' ') {
+                words++;
+            }
+        }
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) == '.'|| word.charAt(i) == '!' || word.charAt(i) == '?') {
+                sentances++;
+            }
+        }
+        words++;
+        S=((double)(sentances)/words*100);
+     
+        L=((double)word.replaceAll("[^a-zA-Z]", "").length())/words*100;
+        index= (int)((0.0588 * L) - (0.296 * S) - 15.8);
+        System.out.println(index);
         
 
     }
